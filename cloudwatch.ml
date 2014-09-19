f14048d9403004ecd0a22b57e84950823bbf9e2f
Martin Jambon <martin@mjambon.com>
2014-09-18 18:10:06 -0700

    Treat data points without a value differently. Data points with values automatically result in 2 metrics (.rate and .average).

--- cloudwatch.ml
+++ cloudwatch.ml
@@ -1,5 +1,13 @@
 open Lwt
 
+let send0 key opt_value =
+  let conf = Config.get () in
+  let open Config_t in
+  Gator_client.send
+    ~host: conf.gator_host
+    ~port: conf.gator_port
+    key opt_value
+
 (*
    Send a (key, value) pair to Amazon Cloudwatch via our gator service,
    which sums up values over 1-min periods before sending them to Cloudwatch.
@@ -9,17 +17,13 @@ open Lwt
    See gator_client.mli for details, and look at existing metrics
    in our Cloudwatch dashboard.
 
-   The value must be 0. or a positive float.
+   The value must be greater than or equal to 0.
 *)
 let send key value =
-  let conf = Config.get () in
-  let open Config_t in
-  Gator_client.send
-    ~host: conf.gator_host
-    ~port: conf.gator_port
-    key value
+  send0 key (Some value)
 
-let send_event key = send key 1.0
+let send_event key =
+  send0 key None
 
 (*
    Measure a latency and send it to Cloudwatch.
